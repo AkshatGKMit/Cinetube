9ac04c0b7515cf48890c72b5d973060edda26f32
AkshatGKMit <akshat@gkmit.co>
2024-10-23 15:25:23 +0530

    feat: Add configurations and rename app

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@contexts': './src/contexts',
+          '@helpers': './src/helpers',
+          '@hooks': './src/hooks',
+          '@navigation': './src/navigation',
+          '@network': './src/network',
+          '@screens': './src/screens',
+          '@styles': './src/styles',
+          '@themes': './src/themes',
+        },
+      },
+    ],
+  ],
 };
