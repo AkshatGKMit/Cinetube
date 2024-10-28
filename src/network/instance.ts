950139a3279067fbf51858da89f2944d89b641c3
AkshatGKMit <akshat@gkmit.co>
2024-10-28 20:37:36 +0530

    feat: Add axios interceptors

--- src/network/instance.ts
+++ src/network/instance.ts
@@ -1,5 +1,7 @@
 import ApiConstants from '@constants/api';
-import axios from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { ErrorHandler } from './errors';
+import Toast from 'react-native-toast-message';
 
 const { BASE_URL, headers } = ApiConstants;
 
@@ -7,6 +9,27 @@ const instance = axios.create({
   baseURL: BASE_URL,
   headers,
 });
+
+function interceptorResponse<T>(response: AxiosResponse<ApiCallSuccess<T>>): AxiosResponse<ApiCallSuccess<T>> {
+  if (!response.data.success) {
+    throw new Error(response.statusText || 'Unknown error occurred');
+  }
+
+  const apiSuccess: ApiCallSuccess<T> = { success: true, responseData: response.data as T };
+
+  response.data = apiSuccess;
+
+  return response;
+}
+
+function interceptorError(error: AxiosError): ApiCallFailure {
+  const apiError = ErrorHandler(error);
+
+  const apiFailure: ApiCallFailure = { success: false, error: apiError };
+  return apiFailure;
+}
+
+instance.interceptors.response.use(interceptorResponse, interceptorError);
 
 const _get = <T, Params = {}>(url: string, config?: ApiCallParams<Params>): Promise<ApiCallResponse<T>> => {
   return instance.get(url, config);
