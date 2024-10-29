98b3c50f7f07c6acd85bb1f86dc9dd69f64ed1d3
AkshatGKMit <akshat@gkmit.co>
2024-10-29 18:03:59 +0530

    feat: Add error on no internet connection

--- src/screens/AppContainer/AppContainer.tsx
+++ src/screens/AppContainer/AppContainer.tsx
@@ -1,8 +1,21 @@
+import { useEffect } from 'react';
 import { Text, SafeAreaView } from 'react-native';
+import { useNetInfo } from '@react-native-community/netinfo';
 import PlatformStatusBar from '@components/platformStatusBar';
 import globalStyles from '@themes/globalStyles';
 
 const AppContainer = () => {
+  const netInfo = useNetInfo();
+
+  useEffect(() => {
+    console.log(netInfo);
+
+    if (netInfo.isConnected !== null) {
+      const { isConnected } = netInfo;
+      if (!isConnected) throw new Error('No internet connection');
+    }
+  }, [netInfo]);
+
   return (
     <>
       <PlatformStatusBar />
